5f57749ec1eb5b6e83a1c70e0de78fdde76819f7
Ar3missss <pathaniaaryan169@gmail.com>
2026-03-28 18:49:40 +0530

    physics

--- game.py
+++ game.py
@@ -1,30 +1,34 @@
 import sys
 import pygame 
-pygame.init()
+from scripts.utils import load_image
+from scripts.entities import PhysicsEntity
 
-WIDTH=640
-HEIGHT=480
-
-class main:
-
+class game:
+      
     def __init__(self):
+        pygame.init()
+
         self.caption = pygame.display.set_caption("NINJA GAME")
         
-        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        self.screen = pygame.display.set_mode((640,480))
         
         self.clock = pygame.time.Clock()
-        
-        self.image = pygame.image.load("/Users/macbookair/Python/Marioo/data/images/clouds/cloud_1.png")
-
-        self.image_pos = [160,260]
 
         self.movement = [False,False]
+
+        self.assets = {
+            'player' : load_image('entities/player.png')
+        }
+
+        self.player = PhysicsEntity(self, 'player', (50,50),(8,15))
 
     def run(self):             
         while True:
             self.screen.fill((14,219,248))
-            self.image_pos[1] += (self.movement[1] - self.movement[0])*5   ## calculations if we press downkey = 260 += (1 - 0)*5 = 265  
-            self.screen.blit(self.image,self.image_pos)
+
+            self.player.update((self.movement[1]-self.movement[0],0)) 
+
+            self.player.render(self.screen)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -32,22 +36,19 @@ class main:
                     sys.exit()
                 
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = True
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
                 
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = False
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
             
             self.clock.tick(60)
             pygame.display.update()
 
-
-main().run()
-
 if __name__ == "__main__":
-    main()
+    game().run()
---
+++ scripts/entities.py
@@ -0,0 +1,19 @@
+import pygame
+
+class PhysicsEntity:
+    def __init__(self, game, e_type, pos, size):
+        self.game = game
+        self.type = e_type
+        self.pos = list(pos)   # We use list(pos) bcz to make position mutable for eg --> if our Player pos is  Player((100,200))it is immutable it becomes [100,200] now its list and mutable then self.pos[0] += 5
+        self.size = size
+        self.velocity = [0,0]  # Velocity = speed + direction so intially our velocity is [0,0] by speed it means left and right and by direction it means up and down
+        
+    
+    def update(self,movement = (0,0)):
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+
+        self.pos[0] += frame_movement[0]
+        self.pos[1] += frame_movement[1]
+
+    def render(self,surf):
+        surf.blit(self.game.assets['player'],self.pos)
