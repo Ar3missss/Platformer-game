4d71cabe13a98078c0624642474e3cef32491056
Ar3missss <pathaniaaryan169@gmail.com>
2026-03-29 15:48:42 +0530

    xd

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
 import sys
 import pygame 
-from scripts.utils import load_image
+from scripts.utils import load_image , load_images 
 from scripts.entities import PhysicsEntity
+from scripts.tilemap import Tilemap
 
-class game:
+class Game:
       
     def __init__(self):
         pygame.init()
@@ -11,24 +12,39 @@ class game:
         self.caption = pygame.display.set_caption("NINJA GAME")
         
         self.screen = pygame.display.set_mode((640,480))
+
+        # We use .Surface to get a Blank Image here we get a blank image of dimension 320,240
+        # The Idea of creating 2nd display is that we can render on smaller display that is on self.display and then scale it up to the self.screen
+        # We do this create a pixel Art Effect
+
+        self.display = pygame.Surface((320,240))
         
         self.clock = pygame.time.Clock()
 
         self.movement = [False,False]
 
         self.assets = {
+            'decor' : load_images('tiles/decor'),  # ('tiles/decor') this is our path
+            'grass' : load_images('tiles/grass'), 
+            'large_decor' : load_images('tiles/large_decor'), 
+            'spawners' : load_images('tiles/spawners'),
+            'stone' : load_images('tiles/stone'), 
             'player' : load_image('entities/player.png')
         }
 
         self.player = PhysicsEntity(self, 'player', (50,50),(8,15)) # Calling PhyscisEntity Class (self, game, e_type, pos, size)
 
+        self.tilemap = Tilemap(self)
+
     def run(self):             
         while True:
-            self.screen.fill((14,219,248))
+            self.display.fill((14,219,248))
+
+            self.tilemap.render(self.display)
 
             self.player.update((self.movement[1]-self.movement[0],0)) # (x,y)
 
-            self.player.render(self.screen)
+            self.player.render(self.display) # self.player is a physicsEntity object so this object has access to render() and update() methods
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -47,8 +63,9 @@ class game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
             
+            self.screen.blit(pygame.transform.scale(self.display,self.screen.get_size()),(0,0))   
             self.clock.tick(60)
             pygame.display.update()
 
 if __name__ == "__main__":
-    game().run()
+    Game().run()
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,6 @@
+import os
 import pygame
+
 
 
 BASE_IMAGE_PATH = 'data/images/'
@@ -7,3 +9,9 @@ def load_image(path):
     img = pygame.image.load(BASE_IMAGE_PATH + path).convert()
     img.set_colorkey((0,0,0))
     return img
+
+def load_images(path):
+    images = []
+    for img_name in sorted(os.listdir(BASE_IMAGE_PATH + path)): # os.listdir will give you all the images present in the folder like there are 10 images so it will give u all 10 
+        images.append(load_image(path + '/' + img_name))
+        return images 
